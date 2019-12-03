27e9bc58ad3abfc0d728535b44f19c1354f46c05
Amitai Burstein <amitai@gizra.com>
2019-12-03 16:50:07 +0200

    Add Service

---
+++ app/src/Model/GroupMeeting.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Model;
+
+
+class GroupMeeting
+{
+
+    private $name;
+    private $date;
+    private $mothers;
+    private $children;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMothers()
+    {
+        return $this->mothers;
+    }
+
+    /**
+     * @param mixed $mothers
+     */
+    public function setMothers($mothers): void
+    {
+        $this->mothers = $mothers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * @param mixed $children
+     */
+    public function setChildren($children): void
+    {
+        $this->children = $children;
+    }
+
+
+}
--- app/src/Service/GroupMeetingManager.php
+++ app/src/Service/GroupMeetingManager.php
@@ -4,11 +4,39 @@
 namespace App\Service;
 
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
 class GroupMeetingManager implements GroupMeetingManagerInterface
 {
 
+    private $serializer;
+    private $kernel;
+
+    public function __construct(SerializerInterface $serializer, KernelInterface $kernel)
+    {
+        $this->serializer = $serializer;
+        $this->kernel = $kernel;
+    }
+
     public function index()
     {
+
+        $finder = new Finder();
+        $finder->files()->in($this->kernel->getProjectDir() . '/../data/group-meetings');
+
+
+
+        foreach ($finder as $file) {
+            $absoluteFilePath = $file->getRealPath();
+            $fileNameWithExtension = $file->getRelativePathname();
+
+            // ...
+        }
+
+
+
 
         return [
           '2020-01-30-morning' => [
