bb9c043f7d0db3b16427081c26d855a47496b331
Amitai Burstein <amitai@gizra.com>
2019-12-05 22:41:59 +0200

    Save files

--- app/src/Controller/ChildController.php
+++ app/src/Controller/ChildController.php
@@ -40,49 +40,56 @@ class ChildController extends AbstractController
         // meeting.
         $measurementsFromGroupMeetingIndex = null;
         $measurements = $child->getMeasurements() ?: [];
+        /** @var ChildMeasurements $childMeasurements */
+        $childMeasurements = null;
         foreach ($measurements as $index => $measurement) {
             if ($measurement->getGroupMeeting() == $groupMeeting->getFileId()) {
-                $measurementsFromGroupMeetingIndex = $index;
+                $childMeasurements = $measurement;
                 break;
             }
         }
 
-        $form = null;
-        if (!$measurementsFromGroupMeetingIndex) {
-            // Add form to create new measurements.
+        $hasExistingMeasurements = false;
+        if (!$childMeasurements) {
+            // New measurements
             $childMeasurements = new ChildMeasurements();
-
             $childMeasurements->setGroupMeeting($groupMeeting->getFileId());
 
             $now = new \DateTime();
             $fileId = $now->format('Y-m-d-H:i');
             $childMeasurements->setFileId($fileId);
-
-            $form = $this->createForm(ChildMeasurementsType::class, $childMeasurements);
-
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid()) {
-                // $form->getData() holds the submitted values
-                // but, the original `$task` variable has also been updated
-                $childMeasurementsNewData = $form->getData();
-
-                // @todo: Validate.
-
-
-                dump($childMeasurementsNewData);
-                $childMeasurementsManager->create($child->getFileId(), $fileId, $childMeasurementsNewData);
-
-                // Reload page.
-                return $this->redirect($request->getUri());
-            }
         }
+        else {
+            // Existing measurements.
+            $fileId = $childMeasurements->getFileId();
+            $hasExistingMeasurements = true;
+        }
+
+
+        $form = $this->createForm(ChildMeasurementsType::class, $childMeasurements);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $childMeasurementsNewData = $form->getData();
+
+            // @todo: Validate.
+
+            $childMeasurementsManager->create($child->getFileId(), $fileId, $childMeasurementsNewData);
+
+            // Reload page.
+            return $this->redirect($request->getUri());
+        }
+
+
 
 
         return $this->render('child/show.html.twig', [
           'child' => $child,
           'group_meeting' => $groupMeeting,
-          'measurements_from_group_meeting_index' => $measurementsFromGroupMeetingIndex,
-          'form' => $form ? $form->createView() : null,
+          'has_existing_measurements' => $hasExistingMeasurements,
+          'form' => $form->createView(),
         ]);
     }
 }
