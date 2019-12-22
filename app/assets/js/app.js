d50de565d9a87563e6b041ba8bedf6bb39e5f467
Amitai Burstein <amitai@gizra.com>
2019-12-22 20:59:09 +0200

    Add assets

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -5,8 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// any CSS you require will output into a single css file (app.scss in this case)
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
