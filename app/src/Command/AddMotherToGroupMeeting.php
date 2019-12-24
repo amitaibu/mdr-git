51527440663fb9b1b44f0011270d47632914a35a
Amitai Burstein <amitai@gizra.com>
2019-12-24 13:46:32 +0200

    Adapt command

--- app/src/Command/AddMotherToGroupMeeting.php
+++ app/src/Command/AddMotherToGroupMeeting.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\GroupMeetingManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -10,28 +11,45 @@ use Symfony\Component\Console\Question\Question;
 
 class AddMotherToGroupMeeting extends Command
 {
-    protected static $defaultName = 'app:add-mother-to-group-meeting';
+    protected static $defaultName = 'app:get-group-meeting-list';
+
+    private $groupMeetingManager;
+
+    public function __construct(GroupMeetingManagerInterface $groupMeetingManager)
+    {
+        $this->groupMeetingManager = $groupMeetingManager;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
         $this
-          ->setDescription('Add a mother to a group meeting.')
+          ->setDescription('Get listing of a group meeting.')
 
           // the full command description shown when running the command with
           // the "--help" option
-          ->setHelp('Add existing mothers to an existing group meeting.')
+          ->setHelp('Show list of mothers in a group meeting.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $helper = $this->getHelper('question');
+
+        $groupMeetings = $this->groupMeetingManager->index();
+        $groupMeetingsName = [];
+        /** @var \App\Entity\GroupMeeting $groupMeeting */
+        foreach ($groupMeetings as $groupMeeting) {
+            $groupMeetingsName[$groupMeeting->getFileId()] = $groupMeeting->getName();
+        }
+
+
         $question = new ChoiceQuestion(
-          'Please select your favorite color (defaults to red)',
-          ['red', 'blue', 'yellow'],
-          0
+          'Please select a Group meeting',
+          $groupMeetingsName
         );
-        $question->setErrorMessage('Color %s is invalid.');
+        $question->setErrorMessage('Group meeting %s is invalid.');
 
         $color = $helper->ask($input, $output, $question);
         $output->writeln('You have just selected: '.$color);
