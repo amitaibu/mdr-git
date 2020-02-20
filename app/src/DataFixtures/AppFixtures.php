06220ce0bb2a7c434c0764aaa55954efd86e9c34
Amitai Burstein <amitai@gizra.com>
2020-02-20 16:14:53 +0200

    Add fixtures

---
+++ app/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Child;
+use App\Entity\GroupMeeting;
+use App\Entity\GroupMeetingAttendanceList;
+use App\Entity\Mother;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = Faker\Factory::create();
+
+        // Create Mothers and children.
+        $mothers = [];
+        for ($i = 0; $i < 20; $i++) {
+            $mother = new Mother();
+            $mother->setFirstName($faker->firstNameFemale);
+            $mother->setLastName($faker->lastNameFemale);
+            $mother->setBirthdayEstimated(False);
+
+            for ($j = 0; $j < 3; $j++) {
+                $child = new Child();
+                $child->setFirstName($faker->firstNameFemale);
+                $child->setLastName($faker->lastNameFemale);
+                $child->setMother($mother);
+            }
+
+            $manager->persist($mother);
+            $mothers[] = $mother;
+        }
+
+        // Create group meetings.
+        $groupMeetings = [];
+        for ($i = 0; $i < 3; $i++) {
+            $groupMeeting = new GroupMeeting();
+            $groupMeeting->setName('product '.$i);
+            $groupMeeting->setDate($faker->dateTime);
+
+            $manager->persist($groupMeeting);
+            $groupMeetings[] = $groupMeeting;
+        }
+
+        // Create group meetings attendance list.
+        $counter = 0;
+        foreach ($mothers as $mother) {
+            $groupMeetingAttendanceList = new GroupMeetingAttendanceList();
+            $groupMeetingAttendanceList->setMother($mother);
+            $groupMeetingAttendanceList->setGroupMeeting($groupMeetings[$counter]);
+
+            $counter++;
+            if ($counter > count($groupMeetings)) {
+                $counter = 0;
+            }
+
+            $manager->persist($groupMeetingAttendanceList);
+        }
+
+        $manager->flush();
+    }
+}
--- app/src/Entity/Mother.php
+++ app/src/Entity/Mother.php
@@ -31,6 +31,16 @@ class Mother
      */
     private $children;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
     public function __construct()
     {
         $this->children = new ArrayCollection();
@@ -81,6 +91,30 @@ class Mother
                 $child->setMother(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
