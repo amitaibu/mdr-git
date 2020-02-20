65595e6a56ee701281a02c6f71a6b5d006612b78
Amitai Burstein <amitai@gizra.com>
2020-02-20 13:47:21 +0200

    Add ChildMeasurements

--- app/src/Entity/Child.php
+++ app/src/Entity/Child.php
@@ -32,6 +32,11 @@ class Child
      */
     private $mother;
 
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\ChildMeasurements", mappedBy="child", cascade={"persist", "remove"})
+     */
+    private $childMeasurements;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +74,23 @@ class Child
     public function setMother(?Mother $mother): self
     {
         $this->mother = $mother;
+
+        return $this;
+    }
+
+    public function getChildMeasurements(): ?ChildMeasurements
+    {
+        return $this->childMeasurements;
+    }
+
+    public function setChildMeasurements(ChildMeasurements $childMeasurements): self
+    {
+        $this->childMeasurements = $childMeasurements;
+
+        // set the owning side of the relation if necessary
+        if ($childMeasurements->getChild() !== $this) {
+            $childMeasurements->setChild($this);
+        }
 
         return $this;
     }
---
+++ app/src/Entity/ChildMeasurements.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ChildMeasurementsRepository")
+ */
+class ChildMeasurements
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Child", inversedBy="childMeasurements", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $child;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $height;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChild(): ?Child
+    {
+        return $this->child;
+    }
+
+    public function setChild(Child $child): self
+    {
+        $this->child = $child;
+
+        return $this;
+    }
+
+    public function getWeight(): ?string
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(string $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(float $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+}
